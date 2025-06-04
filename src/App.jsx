b1aec2d89146ce3bc1b07f8314d7f196223b0cf3
Vania-0731 <sonalysifuentes@gmail.com>
2025-06-04 16:43:33 -0500

    v3

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import Contact from "./pages/Contacto"; 
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className="div class new-hover-card">
-      <h1 className="border-bottom pb-3 mb-3">Dribble</h1>
-      <div className="row">
-        <div className="col-md-4">
-          <h1 class="text-align-center text-size-20 text-size-32-ns text-weight-700 mb10">
-  Gallery
-</h1>
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">sd</h5>
-              <p className="card-text">Comedy</p>
-              <button className="btn btn-primary">Description</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/items" element={<Items />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </>
+  );
 }
 
-
-export default App
+export default App;
