4c3621f04308cefa1f18345ff07f0e9fe2d7a5bd
Vania-0731 <sonalysifuentes@gmail.com>
2025-06-04 16:31:21 -0500

    v1

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <div className="div class new-hover-card">
+      <h1 className="border-bottom pb-3 mb-3">Dribble</h1>
+      <div className="row">
+        <div className="col-md-4">
+          <h1 class="text-align-center text-size-20 text-size-32-ns text-weight-700 mb10">
+  Gallery
+</h1>
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">sd</h5>
+              <p className="card-text">Comedy</p>
+              <button className="btn btn-primary">Description</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+export default App
